6d54d15fe24717b1eeae82bddd06f14a0dfdd795
Derek Prieur <DPrieur123@gmail.com>
2023-04-23 08:22:15 -0400

    add stripe label

--- frontend/src/components/PaymentForm.jsx
+++ frontend/src/components/PaymentForm.jsx
@@ -47,23 +47,28 @@ const PaymentForm = ({ onStepChange }) => {
     };
 
     return (
-        <form onSubmit={handleSubmit} className="bg-white shadow-md rounded px-4 pt-6 pb-8 mb-4 w-full md:w-1/2 lg:w-1/3 mx-auto">
-            <div className="mb-4">
-                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="card-element">
-                    Credit or Debit Card
-                </label>
-                <div className="bg-gray-100 p-2 rounded">
-                    <CardElement className="w-full p-2" />
+        <div className='px-4'>
+            <form onSubmit={handleSubmit} className="bg-white shadow-md rounded px-4 pt-6 pb-8 mb-4 w-full md:w-1/2 lg:w-1/3 mx-auto mt-16">
+                <div className="mb-4">
+                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="card-element">
+                        Credit or Debit Card
+                    </label>
+                    <div className="bg-gray-100 p-2 rounded">
+                        <CardElement className="w-full p-2" />
+                    </div>
                 </div>
+                <button
+                    type="submit"
+                    disabled={!stripe}
+                    className="bg-medium-brown hover:bg-dark-brown text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full"
+                >
+                    Pay
+                </button>
+            </form>
+            <div className="stripe-footer text-center text-base text-medium-gray mt-4">
+                Powered by <a href="https://stripe.com" target="_blank" rel="noopener noreferrer" className="text-blue-500 underline">Stripe</a>
             </div>
-            <button
-                type="submit"
-                disabled={!stripe}
-                className="bg-medium-brown hover:bg-dark-brown text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full"
-            >
-                Pay
-            </button>
-        </form>
+        </div>
     );
 };
 
