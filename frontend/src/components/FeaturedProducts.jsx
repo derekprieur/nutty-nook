eb1bb7ca238eedecea44c6ff3e15cd7e7dc8bf4d
Derek Prieur <DPrieur123@gmail.com>
2023-04-23 08:12:27 -0400

    loading animation for empty products list

--- frontend/src/components/FeaturedProducts.jsx
+++ frontend/src/components/FeaturedProducts.jsx
@@ -42,38 +42,41 @@ const FeaturedProducts = () => {
     return (
         <div className="w-full mx-auto max-w-sm md:max-w-7xl p-8">
             <h2 className="text-3xl font-bold text-dark-brown mb-4">Featured Products</h2>
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-                {displayedProducts.map(product => {
-                    const cartItem = findCartItem(product.id);
-                    return (
-                        <div key={product.id} className="bg-white rounded-lg p-4 flex flex-col items-center shadow-md hover:shadow-lg transition-shadow duration-300">
-                            <Link to={`/products/${product.id}`}>
-                                <img src={product.images[0]} alt={product.name} className="w-24 h-24 mb-3 object-contain" />
-                            </Link>
-                            <Link to={`/products/${product.id}`}>
-                                <h3 className="text-lg font-bold text-dark-brown mb-2">{product.name}</h3>
-                            </Link>
-                            <p className="text-medium-brown mb-4">${product.price.toFixed(2)}</p>
-                            {cartItem ? (
-                                <div className="flex items-center">
-                                    <button className="bg-light-brown text-white px-2 py-1 rounded-l-lg hover:bg-medium-brown transition-colors duration-300" onClick={() => handleRemoveFromCart(product)}>
-                                        -
+            {!products.length ?
+                <p className='text-dark-brown animate-pulse'>Loading...</p> :
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+                    {displayedProducts.map(product => {
+                        const cartItem = findCartItem(product.id);
+                        return (
+                            <div key={product.id} className="bg-white rounded-lg p-4 flex flex-col items-center shadow-md hover:shadow-lg transition-shadow duration-300">
+                                <Link to={`/products/${product.id}`}>
+                                    <img src={product.images[0]} alt={product.name} className="w-24 h-24 mb-3 object-contain" />
+                                </Link>
+                                <Link to={`/products/${product.id}`}>
+                                    <h3 className="text-lg font-bold text-dark-brown mb-2">{product.name}</h3>
+                                </Link>
+                                <p className="text-medium-brown mb-4">${product.price.toFixed(2)}</p>
+                                {cartItem ? (
+                                    <div className="flex items-center">
+                                        <button className="bg-light-brown text-white px-2 py-1 rounded-l-lg hover:bg-medium-brown transition-colors duration-300" onClick={() => handleRemoveFromCart(product)}>
+                                            -
+                                        </button>
+                                        <input type="number" min="1" value={cartItem.quantity} className="w-10 text-center border-t border-b border-medium-brown" readOnly />
+                                        <button className="bg-light-brown text-white px-2 py-1 rounded-r-lg hover:bg-medium-brown transition-colors duration-300" onClick={() => handleAddToCart(product)}>
+                                            +
+                                        </button>
+                                    </div>
+                                ) : (
+                                    <button className="bg-light-brown text-white px-4 py-2 rounded-lg flex items-center hover:bg-medium-brown transition-colors duration-300" onClick={() => handleAddToCart(product)}>
+                                        <FaShoppingCart className="mr-2" />
+                                        Add to Cart
                                     </button>
-                                    <input type="number" min="1" value={cartItem.quantity} className="w-10 text-center border-t border-b border-medium-brown" readOnly />
-                                    <button className="bg-light-brown text-white px-2 py-1 rounded-r-lg hover:bg-medium-brown transition-colors duration-300" onClick={() => handleAddToCart(product)}>
-                                        +
-                                    </button>
-                                </div>
-                            ) : (
-                                <button className="bg-light-brown text-white px-4 py-2 rounded-lg flex items-center hover:bg-medium-brown transition-colors duration-300" onClick={() => handleAddToCart(product)}>
-                                    <FaShoppingCart className="mr-2" />
-                                    Add to Cart
-                                </button>
-                            )}
-                        </div>
-                    )
-                })}
-            </div>
+                                )}
+                            </div>
+                        )
+                    })}
+                </div>
+            }
             {displayCount < products.length && (
                 <div className="text-center mt-8">
                     <button
